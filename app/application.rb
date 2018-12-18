31cae6d22f7c083710c6470c81f150e0361e5785
Laura Messa Travin <laura.travin@gmail.com>
2018-12-18 22:51:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,16 +3,12 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-
      val = Time.new
-  
-    if val.hour <= 12
+    if val.hour < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
     end
-
     resp.finish
   end
-
 end
